227dd6741647ac249e683f8921f296702c36c363
Dmitriy Barbul <dimabarbul@gmail.com>
2016-02-07 19:18:17 +0200

    Initial commit

---
+++ Calculator.Core.Tests/CalculatorTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator.Core;
+
+namespace Calculator.Core.Tests
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestMethod]
+        public void Calculate_SimpleSum_Calculated()
+        {
+            string formula = "1 + 2";
+            int result = Calculator.Calculate(formula);
+
+            Assert.AreEqual(3, result);
+        }
+    }
+}
---
+++ Calculator.Core/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Core
+{
+    /// <summary>
+    /// Provides functionality to calculate formula values.
+    /// </summary>
+    public static class Calculator
+    {
+        /// <summary>
+        /// Calculates provided formula
+        /// </summary>
+        /// <param name="formula">Formula to calculate.</param>
+        /// <returns>Calculation result.</returns>
+        public static int Calculate(string formula)
+        {
+            return 0;
+        }
+    }
+}
