20f67ec8fe87d2ea9f4186dd01aa662277d8b7b8
Dmitriy Barbul <dimabarbul@gmail.com>
2016-02-08 16:02:26 +0200

    Add console calculator application

---
+++ Calculator.Console/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Core = Calculator.Core;
+
+namespace Calculator.Console
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length == 1 && args[0] != "-h" && args[0] != "--help")
+            {
+                System.Console.WriteLine(@"Usage: {0} [formula]", AppDomain.CurrentDomain.FriendlyName);
+                return;
+            }
+            else if (args.Length == 1)
+            {
+                System.Console.Write(Core.Calculator.Calculate(args[1]));
+            }
+            else
+            {
+                System.Console.Write("Enter formula: ");
+                string formula = System.Console.ReadLine();
+                System.Console.WriteLine("{0} = {1}", formula, Core.Calculator.Calculate(formula));
+            }
+        }
+    }
+}
