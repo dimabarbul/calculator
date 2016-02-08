6e3f841814bbde6ca5e51094ccb6620e807d9f08
Dmitriy Barbul <dimabarbul@gmail.com>
2016-02-08 13:48:29 +0200

    Add tests for Token.ToDecimal method

---
+++ Calculator.Core.Tests/TokenTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calculator.Core.Tests
+{
+    [TestClass]
+    public class TokenTest
+    {
+        [TestMethod]
+        public void ToDecimal_Number_IsCorrectNumber()
+        {
+            Token token = new Token("2", true);
+
+            Assert.AreEqual(true, token.IsNumber);
+            Assert.AreEqual("2", token.Value);
+            Assert.AreEqual(2m, token.ToDecimal());
+        }
+
+        [TestMethod]
+        public void ToDecimal_Number_ValueNotChanged()
+        {
+            Token token = new Token(".3", true);
+
+            Assert.AreEqual(true, token.IsNumber);
+            Assert.AreEqual(".3", token.Value);
+            Assert.AreEqual(0.3m, token.ToDecimal());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void ToDecimal_NotNumberWithDigits_ThrowsException()
+        {
+            Token token = new Token("3", false);
+
+            token.ToDecimal();
+        }
+    }
+}
