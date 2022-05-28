81089c24f19d3e1f46a8d3d54912679f7a983449
Dmytro Barbul <sesa640134@non.se.com>
2022-05-28 18:51:53 +0300

    Add dependency injection project

---
+++ Calculator.Core.DependencyInjection/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using Calculator.Core.Parser;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Calculator.Core.DependencyInjection;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddCalculator(this IServiceCollection services)
+    {
+        Assembly? entryAssembly = Assembly.GetEntryAssembly();
+
+        if (entryAssembly != null)
+        {
+            foreach (Type parserType in entryAssembly.GetReferencedAssemblies()
+                .SelectMany(an => Assembly.Load(an).GetTypes())
+                .Where(t => t.IsAssignableTo(typeof(IParser))))
+            {
+                services.AddScoped(typeof(IParser), parserType);
+            }
+        }
+
+        return services;
+    }
+}
